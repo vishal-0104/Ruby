ba607f432bb84388c83600390b76a9f26cc169af
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2024-11-28 09:44:43 +0530

    first_prog

---
+++ start.rb
@@ -0,0 +1,17 @@
+# print 'hello '
+# puts 'hello again'
+
+# num = 25
+# string = " hello"
+# boolean = true
+# float = 2.5
+
+# puts num
+# puts string
+# puts boolean
+# puts float 
+
+name = "vishal"
+
+puts "hello, #{name}"
+puts "hello, " + name
