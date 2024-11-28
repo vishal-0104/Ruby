5da3aa2665977f0b51cfdf0bf2a811034d5be456
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2024-11-28 14:08:37 +0530

    Strings

---
+++ string.rb
@@ -0,0 +1,14 @@
+puts "Enter Name"
+name = gets.chomp
+
+puts name.upcase()
+puts name.downcase()
+puts name.length()
+
+puts name.include? "ish"
+
+puts name[2]
+
+puts name[0,4]
+
+puts name.index("i")
