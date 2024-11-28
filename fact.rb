15fb327d723fb082d901cffd65d003e9a02db527
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2024-11-28 13:28:17 +0530

    factorial

--- fact.rb
+++ fact.rb
@@ -1,1 +1,9 @@
-n = gets.chomp
+def factorial(n)
+  return 1 if n <= 1
+  n * factorial(n - 1)
+end
+
+puts "Enter a number:"
+num = gets.chomp.to_i
+puts "Factorial of #{num} is #{factorial(num)}"
+
