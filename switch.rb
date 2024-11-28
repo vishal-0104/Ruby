8b9d87026a7a2176a28c56420db4442dc987605b
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2024-11-28 10:37:44 +0530

    case

---
+++ switch.rb
@@ -0,0 +1,20 @@
+str = gets.chomp
+
+case str
+
+when "one"
+  puts "input is one"
+
+when "two"
+  puts "input is two"
+
+when "three"
+  puts "input is three"
+
+when "four"
+  puts "input is four"
+
+else 
+  puts "default"
+
+end
